dbe3caddd4e350ee3d9fa9b17c02b904d55816d7
indecisive-m <michr.watkins@btinternet.com>
2025-05-07 22:22:13 +0100

    Added media repository to koin module

--- app/src/main/java/com/example/whattowatch/di/koinModule.kt
+++ app/src/main/java/com/example/whattowatch/di/koinModule.kt
@@ -3,6 +3,8 @@ package com.example.whattowatch.di
 import com.example.whattowatch.data.network.HttpClientFactory
 import com.example.whattowatch.data.network.KtorRemoteDataSource
 import com.example.whattowatch.data.network.RemoteDataSource
+import com.example.whattowatch.data.repository.DefaultMediaRepository
+import com.example.whattowatch.domain.MediaRepository
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -11,4 +13,5 @@ import org.koin.dsl.module
 val koinModule = module {
     single { HttpClientFactory.create(OkHttp.create()) }
     singleOf(::KtorRemoteDataSource).bind<RemoteDataSource>()
+    singleOf(::DefaultMediaRepository).bind<MediaRepository>()
 }
